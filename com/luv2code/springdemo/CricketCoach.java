a798cd16ba90ba862d74ceb3dfb1316015571570
Jupraj <singj12@farmingdale.edu>
2019-05-15 02:48:40 -0400

    first commit

---
+++ com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,49 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+	private FortuneService fortuneService;
+	// add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+	
+	// creating a no-arg constructor
+	public CricketCoach() {
+		System.out.println("CricketCoach: inside the no parameter constructor");
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+
+		this.team = team;
+	}
+
+	// our setter method that will be used by Spring when it injects the dependency
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach: inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 minutes";
+	}
+
+}
