9c39bcb1e26d01fc8796ef701ad35c8b1976a794
Jupraj <juprajsngh1@gmail.com>
2019-05-15 17:59:16 -0400

    added Bean Scope Lifecycles into the mix

---
+++ com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,35 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		// load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		
+		
+		// retrieve beans from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		//close the context
+		context.close();
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- com/luv2code/springdemo/TrackCoach.java
+++ com/luv2code/springdemo/TrackCoach.java
@@ -23,6 +23,16 @@ public class TrackCoach implements Coach {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
 
+	//add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrachCoach: inside method doMyStartupStuff");
+		
+	}
+	
+	//add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrachCoach: inside method doMyCleanupStuffYoYo");
+	}
 }
 
 
